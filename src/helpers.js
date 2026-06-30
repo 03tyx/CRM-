086da3ad4a190765cdd2aa202415dfcba6d7e31e
03tyx <tohyanxin03@gmail.com>
2026-06-30 09:21:59 +0800

    update feedback log

--- src/helpers.js
+++ src/helpers.js
@@ -31,6 +31,7 @@ export const FEEDBACK_LOGS = [
   { id: 'FL008', label: "Room Booking 2.0 Feedback Log", url: 'https://docs.google.com/document/d/1m7CbKvfBPKlQuoF8vZiN7vzqzwtOje0YBYnQUviqBpA/edit?usp=sharing' },
   { id: 'FL009', label: "REI 2.0 Feedback Log", url: 'https://docs.google.com/document/d/1R-UI1FamA0Y2dnaKkPfceVjSyfWAzUwaKaa057Sy90A/edit?usp=sharing' },
   { id: 'FL010', label: "GI Project Part 1 Feedback Log", url: 'https://docs.google.com/document/d/19zXrMCUdEAJ024FQNj_8jMamfpsGjtsXUG8ABZwKD9Q/edit?tab=t.u6pn7293rju4' },
+  { id: 'FL011', label: "Advisor Onboarding Part 2 Feedback Log", url: 'https://docs.google.com/document/d/121elOPWhYTjrNbq4ybjgU8-RK1hqfkXlucJ36GAsHpk/edit?usp=sharing' },
 ]
 
 //remove 'Delayed' change 'Upcoming' to red colour
